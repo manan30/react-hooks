38d9549da498a9ba043068e92822e9eb796165c5
Manan Joshi <mananjoshi1995@gmail.com>
2022-05-24 01:33:27 -0500

    feat: adding prettierrc

--- packages/eslint-config-elegante/.eslintrc.js
+++ packages/eslint-config-elegante/.eslintrc.js
@@ -2,25 +2,25 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  plugins: ["react", "react-hooks", "@typescript-eslint", "prettier"],
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
   rules: {
-    "no-console": "warn",
-  },
+    'no-console': 'warn'
+  }
 };
--- packages/eslint-config-elegante/index.js
+++ packages/eslint-config-elegante/index.js
@@ -1,3 +1,3 @@
-const eslintConfig = require("./.eslintrc.js");
+const eslintConfig = require('./.eslintrc.js');
 
 module.exports = eslintConfig;
